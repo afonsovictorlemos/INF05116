bc820f5ce44e37aa1c09497f05ede598e3ed8c45
Afonso Victor Lemos da Silva <afonsovictor@proton.me>
2022-11-03 18:01:15 +0000

    Second question functions.

--- trabalho_1/general.py
+++ trabalho_1/general.py
@@ -29,3 +29,17 @@ def is_prime(n: int, p: list) -> bool:
         elif n % x == 0:
             return False
     return True  # If the list is empty, then the number is prime
+
+
+def list_of_primes(arg: int) -> list:
+    primes: list = []
+    for i in range(2, arg):
+        if is_prime(i, primes):
+            primes.append(i)
+    return primes
+
+
+def quadratic_euler(x: int, y: int, z: int) -> int:
+    number: int = z ** 2 + x * z + y  # n^2 + an + b
+    number: int = abs(number)
+    return number
