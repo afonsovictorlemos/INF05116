61fb15e6f516e3c2af7b2fc680ab967f9c6f4b5f
Afonso Victor Lemos da Silva <afonsovictor@proton.me>
2022-11-03 17:34:56 +0000

    First question functions.

---
+++ trabalho_1/general.py
@@ -0,0 +1,31 @@
+"""
+All functions must be here.
+"""
+
+
+def reversed_number(arg: int) -> int:
+    arg: str = str(arg)  # Changing int into str
+    arg: str = arg[::-1]  # Reversing str
+    arg: int = int(arg)  # Changing str into int
+    return arg  # return int(str(arg).__reversed__)
+
+
+def is_palindrome(arg: int) -> bool:
+    par: int = reversed_number(arg)
+    return arg == par
+
+
+def is_prime(n: int, p: list) -> bool:
+    """
+    It checks if a 'n' number is in the 'p' list of prime numbers.
+    :param n: int
+    :param p: list
+    :return: bool
+    """
+    for x in p:
+        q: int = n // x
+        if x >= q:  # x * q = n = q * x
+            return True
+        elif n % x == 0:
+            return False
+    return True  # If the list is empty, then the number is prime
